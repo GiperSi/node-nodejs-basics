ee710951ad0c2f53d70bf95ebf5824a952c9b031
Dzmitry <74104111+GiperSi@users.noreply.github.com>
2023-06-19 17:37:15 +0300

    Update create.js
    First try

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+const fs = require('fs');
 const create = async () => {
-    // Write your code here 
+    const filePath = 'files/fresh.txt';
+    
+    try {
+        await fs.promises.access(filePath);
+        throw new Error('FS oper fail..');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.promises.writeFile(filePath, 'I am fresh and young');
+            console.log('File created +');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
